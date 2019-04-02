51d980562d3c81be0b8082a8d0c214103c596653
aProperFox <tolson.louis@gmail.com>
2019-04-01 19:48:45 -0600

    break studf

--- lib/pages/retro_board.dart
+++ lib/pages/retro_board.dart
@@ -47,18 +47,20 @@ class _RetroBoardPageState extends State<RetroBoardPage>
   @override
   void initState() {
     controller = new PageController(initialPage: 0);
-    retroBoardRepo = LocalRetroBoardRepo(widget.boardId);
+    initItems();
     super.initState();
   }
 
-  Future<List<Category>> getCategories() async {
-    retroBoard = await retroBoardRepo.getRetroBoard();
-    return retroBoard.columns;
+  void initItems() async {
+    retroBoardRepo = LocalRetroBoardRepo(widget.boardId);
+    final board = await retroBoardRepo.getRetroBoard();
+    setState(() {
+      retroBoard = board;
+    });
   }
 
-  Future<List<BottomNavigationBarItem>> getItems() async {
-    final columns = await getCategories();
-    return columns.map((category) {
+  List<BottomNavigationBarItem> getItems(List<Category> items) {
+    return items.map((category) {
       return BottomNavigationBarItem(
         icon: Icon(category.icon),
         title: Text(category.name),
@@ -67,26 +69,20 @@ class _RetroBoardPageState extends State<RetroBoardPage>
     }).toList();
   }
 
-  FutureBuilder<List<BottomNavigationBarItem>> getBottomNavBar() {
-    return FutureBuilder(
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return BottomNavigationBar(
-            items: snapshot.data,
-            type: BottomNavigationBarType.shifting,
-            currentIndex: _selectedIndex,
-            onTap: (int index) => setState(() {
-                  controller.animateToPage(index,
-                      duration: new Duration(milliseconds: 300),
-                      curve: Curves.easeInOut);
-                  _selectedIndex = index;
-                }),
-          );
-        } else {
-          return Container();
-        }
-      },
-      future: getItems(),
+  Widget getBottomNavBar() {
+    if (retroBoard == null) {
+      return null;
+    }
+    return BottomNavigationBar(
+      items: getItems(retroBoard.columns),
+      type: BottomNavigationBarType.shifting,
+      currentIndex: _selectedIndex,
+      onTap: (int index) => setState(() {
+            controller.animateToPage(index,
+                duration: new Duration(milliseconds: 300),
+                curve: Curves.easeInOut);
+            _selectedIndex = index;
+          }),
     );
   }
 
@@ -125,41 +121,36 @@ class _RetroBoardPageState extends State<RetroBoardPage>
     }
   }
 
-  FutureBuilder<List<Category>> getPageView() {
-    return FutureBuilder(
-      builder: (context, snapshot) {
-        if (!snapshot.hasData) {
-          return Container();
-        }
-        return new PageView(
-          onPageChanged: (int index) {
-            setState(() {
-              _selectedIndex = index;
-            });
-          },
-          controller: controller,
-          children: snapshot.data.map((category) {
-            return Column(
-              children: category.items.map((item) {
-                var itemView = RetroItemView(
-                  item.description,
-                  category.color,
-                  category.name,
-                );
-                itemView.addOnLongPressListener(() {
-                  showDeleteDialog(itemView, () async {
-                    final newRetroBoard = await retroBoardRepo.removeItem(item);
-                    setState(() {
-                      retroBoard = newRetroBoard;
-                    });
-                  });
-                });
-              }).toList(),
+  Widget getPageView() {
+    if (retroBoard == null) {
+      return null;
+    }
+    return PageView(
+      onPageChanged: (int index) {
+        setState(() {
+          _selectedIndex = index;
+        });
+      },
+      controller: controller,
+      children: retroBoard.columns.map((category) {
+        Column(
+          children: category.items.map((item) {
+            var itemView = RetroItemView(
+              item.description,
+              category.color,
+              category.name,
             );
+            itemView.addOnLongPressListener(() {
+              showDeleteDialog(itemView, () async {
+                final newRetroBoard = await retroBoardRepo.removeItem(item);
+                setState(() {
+                  retroBoard = newRetroBoard;
+                });
+              });
+            });
           }).toList(),
         );
-      },
-      future: getCategories(),
+      }).toList(),
     );
   }
 }
